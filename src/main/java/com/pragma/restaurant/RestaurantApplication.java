51d3e04dc00ddebe8202cf1c858850671f371011
Sebastian Ortega <sebastiankay0731@gmail.com>
2023-03-15 19:44:49 -0500

    Se retira clase interceptor innecesaria

--- src/main/java/com/pragma/restaurant/RestaurantApplication.java
+++ src/main/java/com/pragma/restaurant/RestaurantApplication.java
@@ -1,12 +1,8 @@
 package com.pragma.restaurant;
 
-import com.pragma.restaurant.application.mapper.feign.SecurityFeignRequestInterceptor;
-import feign.Logger;
-import feign.RequestInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,8 +12,4 @@ public class RestaurantApplication {
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
 
-	/*@Bean
-	public RequestInterceptor securityFeignRequestInterceptor() {
-		return new SecurityFeignRequestInterceptor();
-	}*/
 }
